664ee397482f8910cd972a2b70e5694703b4513a
白桃乌龙 <1253102853@qq.com>
2022-04-16 14:39:03 +0800

    IU-2021.3.1 <白桃乌龙@BTWL-LAPTOP Create baseRefactoring.xml, _@user_Arc Dark _Material_.icls, IntelliLang.xml, ide.general.xml, mybatisx.xml, git.xml, ide-features-trainer.xml, project.default.xml, rainbow_brackets.xml, Enum.java, yiiguxing.translation.xml, shared-indexes.xml, _@user_Material Deep Ocean.icls, find.xml, filetypes.xml, keymap.xml, colors.scheme.xml, _@user_Material Oceanic.icls, KeyPromoterXSettings.xml, notifications.xml, _@user_Darcula.icls, _@user_SynthWave _84 _Material_.icls, databaseDrivers.xml, github.xml, Windows Proper Redo.xml, laf.xml, _@user_Xcode-Dark.icls, _@user_Material Darker.icls, Class.java, jdk.table.xml, _@user_Solarized Dark _Material_.icls, vcs.xml, grazie_global.xml, _@user_GitHub Dark _Material_.icls, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,8 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author q2333
+ * @date ${YEAR}/${MONTH}/${DAY} ${HOUR}:${MINUTE}
+ **/
+public enum ${NAME} {
+}
