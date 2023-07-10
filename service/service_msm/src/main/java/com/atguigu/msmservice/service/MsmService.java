e647c12cf15d5ed68203099363eba0dd9d513bbd
btwl_y9_win10 <1253102853@qq.com>
2023-07-10 11:14:51 +0800

    IU-2022.2.4 <btwl_y9_win10@btwl-y9-win10 Overwrite remote https://github.com/q2333gh/brilliant.git to local

--- service/service_msm/src/main/java/com/atguigu/msmservice/service/MsmService.java
+++
@@ -1,9 +0,0 @@
-package com.btwl.msmservice.service;
-
-import java.util.Map;
-
-public interface MsmService {
-
-  //发送短信的方法
-  boolean send(Map<String, Object> param, String phone);
-}
--- service/service_oss/src/main/java/com/btwl/oss/service/OssService.java
+++
@@ -1,9 +0,0 @@
-package com.btwl.oss.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface OssService {
-
-  //上传头像到oss
-  String uploadFileAvatar(MultipartFile file);
-}
